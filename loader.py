4fad6b8cd99f9527fbf4a6e3ae81da174a989a72
Shanshan- <shanon5656@gmail.com>
2018-02-28 16:48:38 -0500

    works with test files, and loads to dictionary

--- loader.py
+++ loader.py
@@ -1,25 +1,36 @@
-import os, copy
+import os
+from nat_sorter import sort_natural
 
-netIDs = []
-index = 0
-for path, subdirs, files in os.walk("./collected/"):
-    if subdirs and not netIDs:
-        print(subdirs)
-        netIDs = copy.deepcopy(subdirs)
-    elif index > 6:
-        # generate output file
-        outline = "NetID: " + netIDs[index] + "\n==========\n"
-        outline += "C program: 1/1\nC screenshot: 1/1\n"
-        outline += "Javascript program: 1/1\nJavascript screenshot: 1/1\n"
-        outline += "Python program: 1/1\nPython screenshot: 1/1\n"
-        outline += "Prolog program: 1/1\nProlog screenshot: 1/1\n"
-        outline += "SML program: 1/1\nSML screenshot: 1/1\n"
-        outline += "==========\nTotal Points: 10/10\n"
+class entry:
+    def __init__(self, ppath, name, chapter, page):
+        self.path = ppath
+        self.name = name
+        self.chapter = chapter
+        self.page = page
+        self.index = str(chapter) + "." + str(page)
 
-        # bookkeeping and create file
-        index += 1
-        f = open(path + "/output.txt", "w")
-        f.write(outline)
-        f.close()
-    else:
-        index += 1
+cmcpath = "./img"
+filenames = dict()
+nested = False
+cnum = 0
+pnum = 1
+for path, subdirs, files in os.walk(cmcpath):
+    #case 1: files are all in single folder (aka no subdirs)
+    if not subdirs:
+        cnum += 1
+        sort_natural(files)
+        for file in files:
+            curpath = path + "/" + file
+            tmp = entry(curpath, file, cnum, pnum)
+            filenames[tmp.index] = tmp
+            pnum += 1
+        pnum = 1
+
+    #case 2: files are all divided by chapters (only subdirs)
+    elif not files:
+        nested = True
+
+    #case 3: hybrid of two
+
+for key in filenames:
+    print(str(filenames[key].index) + ", " + filenames[key].name)
