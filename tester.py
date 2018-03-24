c4d7d29466ffaa276c1ffb5ba7161e1dddd4eb49
Shanshan- <shanon5656@gmail.com>
2018-03-24 15:14:57 -0400

    adding other test code

--- tester.py
+++ tester.py
@@ -1,30 +1,30 @@
 from tkinter import *
 from main import main_func
+from loader import loadFiles
 
 def func1():
     print("func1 was called")
-
-def func2():
-    print("func2 was called")
 
 class MainWindow(Frame):
     counter = 0
     def __init__(self, *args, **kwargs):
         Frame.__init__(self, *args, **kwargs)
-        #self.new_button = Button(self, text="Create new window", command=self.create_window)
-        #self.new_button.pack(side="top", pady=20)
-        self.f1_button = Button(self, text="F1", command=func1).pack(side=TOP)
-        self.hi_button = Button(self, text="HI", command=self.say_hi).pack(side=RIGHT)
-        self.f2_button = Button(self, text="F2", command=main_func).pack(side=BOTTOM)
-        self.new_button = Button(self, text="NEW", command=self.create_window).pack(side=LEFT)
-
+        Button(self, text="HI", command=self.say_hi).pack(side=RIGHT)
+        Button(self, text="NEW", command=self.create_window).pack(side=LEFT)
+        Button(self, text="MAIN", command=lambda:main_func()).pack(side=BOTTOM)
+        Button(self, text="LF", command=lambda:print(loadFiles("./img/extras"))).pack(side=TOP)
+        self.pack(padx=20, ipady=5, pady=10)
 
     def create_window(self):
         self.counter += 1
         t = Toplevel(self)
+        t.grab_set() #deactivate other window
         t.wm_title("Window #%s" % self.counter)
-        l = Label(t, text="This is window #%s" % self.counter)
-        l.pack(side="top", fill="both", expand=True, padx=100, pady=100)
+        f = Frame(t) #for padding purposes
+        l = Label(f, text="This is window #%s" % self.counter).pack(side=TOP)
+        Button(f, text="QUIT", fg="red", command=t.destroy).pack(side=BOTTOM)
+            #create a button to close top level
+        f.pack(side="top", fill="both", expand=True, padx=50, pady=50)
 
     def say_hi(self):
         print("Window says hi!")
