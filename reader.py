1b5e1c7b5acd71efb14c2dcd158774a7e496ef04
Shanshan- <shanon5656@gmail.com>
2018-03-24 17:39:19 -0400

    created a menu bar

--- reader.py
+++ reader.py
@@ -1,29 +1,57 @@
 # using guide from http://effbot.org/tkinterbook/tkinter-classes.htm
 from tkinter import *
 
-class App:
-    def __init__(self, master):
-        frame = Frame(master)
-        frame.pack(padx=100, pady=20)
-        self.counter = 0
+def genMenu(rootFrame):
+    #ETC: look up tkinter Menu, MenuButton
+    menuBar = Menu(rootFrame)
+    rootFrame.config(menu=menuBar)
 
-        self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+    # File Options
+    fileOpts = Menu(menuBar, tearoff=0)
+    recentFiles = Menu(fileOpts, tearoff=0)
+    fileOpts.add_command(label="Scrape New", command=NONE)
+    fileOpts.add_separator()
+    fileOpts.add_cascade(label="Open Recent", menu=recentFiles)
+    recentFiles.add_command(label="<no recent files>", state="disabled")
+    fileOpts.add_command(label="Open New", command=NONE)
+
+    # Viewer Options
+    viewOpts = Menu(menuBar, tearoff=0)
+    viewOpts.add_command(label="<to be made>", command=NONE, state="disabled")
+    viewOpts.add_command(label="Quit", command=rootFrame.destroy)
+
+    # Put it all together and return
+    menuBar.add_cascade(label="File Options", menu=fileOpts)
+    menuBar.add_cascade(label="Viewer Options", menu=viewOpts)
+    return menuBar
+
+class Viewer:
+    def __init__(self, master):
+        self.frame = Frame(master)
+        self.frame.pack() # to make the viewer visible
+
+        #create image section
+        #TODO: look up tkinter Scrollbar
+
+        Label(self.frame, text="Test label").pack()
+        self.button = Button(self.frame, text="QUIT", fg="red", command=self.frame.quit)
         self.button.pack(side=LEFT) # item will be placed as far left as possible; default is TOP
 
-        Button(frame, text="Hello", command=self.say_hi).pack(side=LEFT)
+        Button(self.frame, text="Hello", command=self.say_hi).pack(side=LEFT)
 
     def say_hi(self):
         print("Window says hi!")
+        t = Toplevel(self.frame)
 
 if __name__ == "__main__":
     # create the root window which will hold all objects
     root = Tk()
-    root.wm_title("Base window")
-
-    # create objects to go into the root window
-    w = Label(root, text="Hello world!\nThis is a test!")
-    w.pack() # fit this widget to its contents and become visible
-    app = App(root)
+    sheight = root.winfo_screenheight()
+    swidth = root.winfo_screenwidth()
+    root.geometry('%sx%s' % (int(swidth/2), int(sheight/2)))
+    root.wm_title("WCV Reader")
+    genMenu(root)
+    app = Viewer(root)
 
     # start program and open window
     root.mainloop()
